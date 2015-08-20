fdf0ef3a9dc6e0eec2924b69bb914dee8898c1f6
Rod Machen <mail@rodmachen.com>
2015-08-20 13:42:28 -0500

    Initial commit and Functional Stack

---
+++ sprint-one/src/functional/stack.js
@@ -0,0 +1,27 @@
+var Stack = function(){
+  var count = 0;
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  // Implement the methods below
+  someInstance.push = function(value){
+    count++;
+    storage[count] = value;
+  };
+
+  someInstance.pop = function(){
+    if (count) {
+      var popped = storage[count]
+      count--;
+      return popped;
+    }
+  };
+
+  someInstance.size = function(){
+    return count;
+  };
+
+  return someInstance;
+};
